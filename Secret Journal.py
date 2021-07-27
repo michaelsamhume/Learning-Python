7c812d0e91e133c1f40b6b2c85f700f3094aa540
Michael <michaelsamhume>
2021-07-27 12:27:57 -0700

    WOrking secret journal.

--- Secret Journal.py
+++ Secret Journal.py
@@ -1,29 +1,63 @@
-def check_password(test):
-    #Is the password long enough? Does it have letters and numbers?
-    if test == 'JohnCena1':
-        print('Hello Michael')
-        return True
+def set_password(journal):
+    if len(journal[0]) > 0:
+        print('This journal already has a password.')
+        menu(journal)
     else:
-        print('Incorrect password')
-        return False
+        journal[0] = input('Set a password for your journal.')
+        menu(journal)
 
 
+def change_password(journal):
+    pwd = input('Please enter current password.')
+    if pwd == journal[0]:
+        journal[0] = input('Please enter a new password.')
+        menu(journal)
+    else:
+        print('Password incorrect.')
+        menu(journal)
 
 
-    def change_password(officialpassword):
-        pwd = input('What is the current password?')
-        if pwd == officialpassword:
-            officialpassword = input('Pick your new password.')
-        return officialpassword
-    print('Bad password')
-    return False
+def set_message(journal):
+    pwd = input('Please enter current password.')
+    if pwd == journal[0]:
+        journal[1] = input('Please enter desired message.')
+    if len(journal[1]) > 0:
+        print('message successful')
+    menu(journal)
 
-officialPassword = 'blahblah!'
 
-pwd = input('Pick a password.\n')
-if len(pwd)>8:
-    good = check_password(pwd)
-else:
-    good = False
-    print('Password too short.')
-print('Password is good:' +str(good))
+def read_message(journal):
+    pwd = input('Please enter current password.')
+    if pwd == journal[0]:
+        if len(journal[1]) > 0:
+            print(journal[1])
+        menu(journal)
+    else:
+        print('No available Entry')
+        menu(journal)
+
+
+def menu(journal):
+    option = int(input('1) Set password.\n'
+                       '2) Change password\n'
+                       '3) Set message \n'
+                       '4) Read message \n'
+                       '5) Exit \n'
+                       ))
+    if option == 1:
+        set_password(journal)
+    elif option == 2:
+        change_password(journal)
+    elif option == 3:
+        set_message(journal)
+    elif option == 4:
+        read_message(journal)
+    elif option == 5:
+        return
+
+
+if __name__ == '__main__':
+    password = ''
+    message = ''
+    journal = [password, message]
+    menu(journal)
