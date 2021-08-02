8462608ea23e4edb7f0302546c45a35916b309ce
Michael Hume <michaelsamhume@gmail.com>
2021-08-02 15:26:29 -0700

    Started Machine Programming

---
+++ machine programming.py
@@ -0,0 +1,40 @@
+import qcodes as qc
+from qcodes.instrument_drivers.tektronix.Keithley_2450 import Keithley2450
+from qcodes.dataset import initialise_database, Measurement, new_experiment
+from qcodes.dataset.plotting import plot_dataset
+from visa import VisaIOError
+import pylab
+import csv
+
+keithley = Keithley2450("keithley", "GPIB0::10::INSTR")
+keithley.reset()
+keithley.terminals("front")
+
+keithley.source.function("voltage")
+current = keithley.sense.function("current")
+curList=[]
+volts=0
+for volts in range(40):
+    volts += 0.05
+    with keithley.output_enabled.set_to(True):
+        keithley.source.voltage(volts)
+        current = keithley.sense.current()
+        curList.append(current)
+        print(f'Current is {current} for voltage {volts}')
+       # foo = input('<Enter> to continue.')
+print(curList)
+pylab.plot(curList)
+pylab.show()
+
+
+
+
+# keithley.source.function("current")
+# keithley.source.current(1E-6)  # Put 1uA through the resistor
+# current_setpoint = keithley.source.current()
+# voltage = keithley.sense.function("voltage")
+# with keithley.output_enabled.set_to(True):
+#     voltage = keithley.sense.voltage()
+#     me = input('foio')
+# print(f"Approx. resistance: ", voltage / current_setpoint)
+
